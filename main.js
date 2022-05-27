d7ab148738bb9ea9bc6261444fbc61a202e6a471
Ashish K <ashishskabade@gmail.com>
2022-05-27 22:43:06 +0530

    functionalities

--- main.js
+++ main.js
@@ -3,13 +3,24 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
-if(userList.value===undefined){
+if(userList.value===undefined && localStorage.length!=0){
     let obj_deser=JSON.parse(localStorage.getItem('myobj'));
-    userList.appendChild(document.createElement('li').appendChild(document.createTextNode(obj_deser.name+ ' : ' + obj_deser.email)));
-
+    var deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('X'));
+    var li= document.createElement('li');
+    li.appendChild(document.createTextNode(obj_deser.name+ ' : ' + obj_deser.email));
+    li.appendChild(deleteBtn)
+    userList.appendChild(li);
 }
-myForm.addEventListener('submit', onSubmit);
 
+myForm.addEventListener('submit', onSubmit);
+userList.addEventListener('click', del);
+function del(e){
+    console.log(e.target);
+    localStorage.removeItem('myobj');
+    var li = e.target.parentElement;
+    userList.removeChild(li); 
+}
 function onSubmit(e) {
   e.preventDefault();
   
@@ -19,14 +30,18 @@ function onSubmit(e) {
     setTimeout(() => msg.remove(), 3000);
   } else {
     const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    const del= document.createElement('button');
+    del.textContent='X';
+    li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value}`));
+    li.appendChild(del);
     userList.appendChild(li);
+    
     let myobj={
         name:nameInput.value,
         email:emailInput.value
     };
     let obj_ser=JSON.stringify(myobj);
-    localStorage.setItem('myobj',obj_ser);
+    localStorage.setItem(obj_ser,obj_ser);
     
 
 
